0fcc87f68f73c6c5662acb73bd10330d64a1bfae
muhammad230 <ali.khansmr3@gmail.com>
2026-04-16 15:20:56 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import react from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 class="text-3xl font-bold underline text-amber-200">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
